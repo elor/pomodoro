6e79dbd84ff3ddc7f87722a38a850f6bf02bac94
Erik E. Lorenz <erik.lorenz@enas.fraunhofer.de>
2019-02-14 08:26:02 +0100

    Build

--- pomodoro/precache-manifest.349a3d41a9c003456cd61c4f87465ada.js
+++ pomodoro/precache-manifest.349a3d41a9c003456cd61c4f87465ada.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
-    "url": "/pomodoro/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/pomodoro/fonts/Roboto-RegularItalic.4357beb8.woff2"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
@@ -16,6 +16,10 @@ self.__precacheManifest = [
     "url": "/pomodoro/css/chunk-vendors.d68258a7.css"
   },
   {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/pomodoro/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
     "revision": "e79bfd88537def476913f3ed52f4f4b3",
     "url": "/pomodoro/fonts/MaterialIcons-Regular.e79bfd88.eot"
   },
@@ -24,20 +28,20 @@ self.__precacheManifest = [
     "url": "/pomodoro/fonts/MaterialIcons-Regular.012cf6a1.woff"
   },
   {
-    "revision": "570eb83859dc23dd0eec423a49e147fe",
-    "url": "/pomodoro/fonts/MaterialIcons-Regular.570eb838.woff2"
-  },
-  {
     "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
     "url": "/pomodoro/fonts/MaterialIcons-Regular.a37b0c01.ttf"
   },
   {
-    "revision": "ad538a69b0e8615ed0419c4529344ffc",
-    "url": "/pomodoro/fonts/Roboto-Thin.ad538a69.woff2"
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/pomodoro/fonts/Roboto-Thin.d3b47375.woff"
   },
   {
     "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
     "url": "/pomodoro/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/pomodoro/fonts/Roboto-Thin.ad538a69.woff2"
   },
   {
     "revision": "8a96edbbcd9a6991d79371aed0b0288e",
@@ -48,40 +52,52 @@ self.__precacheManifest = [
     "url": "/pomodoro/fonts/Roboto-LightItalic.e8eaae90.woff2"
   },
   {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/pomodoro/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
     "revision": "73f0a88bbca1bec19fb1303c689d04c6",
     "url": "/pomodoro/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/pomodoro/fonts/Roboto-LightItalic.13efe6cb.woff"
   },
   {
     "revision": "35b07eb2f8711ae08d1f58c043880930",
     "url": "/pomodoro/fonts/Roboto-Regular.35b07eb2.woff"
   },
   {
-    "revision": "4357beb823a5f8d65c260f045d9e019a",
-    "url": "/pomodoro/fonts/Roboto-RegularItalic.4357beb8.woff2"
-  },
-  {
-    "revision": "1d6594826615607f6dc860bb49258acb",
-    "url": "/pomodoro/fonts/Roboto-Medium.1d659482.woff"
-  },
-  {
-    "revision": "f5902d5ef961717ed263902fc429e6ae",
-    "url": "/pomodoro/fonts/Roboto-RegularItalic.f5902d5e.woff"
-  },
-  {
-    "revision": "193575e2d8ea3e3f8ec6",
-    "url": "/pomodoro/js/app.8dad438d.js"
+    "revision": "ae65edc4180829ccf3a3",
+    "url": "/pomodoro/js/app.6d00be0e.js"
   },
   {
     "revision": "d8d2dcb6678b058fe51f",
     "url": "/pomodoro/js/chunk-vendors.abdd0bc0.js"
   },
   {
-    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
-    "url": "/pomodoro/fonts/Roboto-Bold.b52fac2b.woff2"
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/pomodoro/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/pomodoro/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/pomodoro/fonts/Roboto-Medium.1d659482.woff"
   },
   {
     "revision": "83e114c316fcc3f23f524ec3e1c65984",
     "url": "/pomodoro/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/pomodoro/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/pomodoro/fonts/Roboto-BoldItalic.94008e69.woff2"
   },
   {
     "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
@@ -96,8 +112,8 @@ self.__precacheManifest = [
     "url": "/pomodoro/fonts/Roboto-Black.59eb3601.woff2"
   },
   {
-    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
-    "url": "/pomodoro/fonts/Roboto-BoldItalic.94008e69.woff2"
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/pomodoro/fonts/Roboto-BlackItalic.f75569f8.woff2"
   },
   {
     "revision": "313a65630d341645c13e4f2a0364381d",
@@ -108,31 +124,15 @@ self.__precacheManifest = [
     "url": "/pomodoro/fonts/Roboto-BlackItalic.cc2fadc3.woff"
   },
   {
-    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
-    "url": "/pomodoro/fonts/Roboto-BlackItalic.f75569f8.woff2"
-  },
-  {
-    "revision": "d3b47375afd904983d9be8d6e239a949",
-    "url": "/pomodoro/fonts/Roboto-Thin.d3b47375.woff"
-  },
-  {
     "revision": "d26871e8149b5759f814fd3c7a4f784b",
     "url": "/pomodoro/fonts/Roboto-Light.d26871e8.woff2"
   },
   {
-    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
-    "url": "/pomodoro/fonts/Roboto-Light.c73eb1ce.woff"
-  },
-  {
-    "revision": "13efe6cbc10b97144a28310ebdeda594",
-    "url": "/pomodoro/fonts/Roboto-LightItalic.13efe6cb.woff"
-  },
-  {
-    "revision": "ce26eb2e56dfb70d3213b9b444164f50",
+    "revision": "07bf3c51a64d79725ce8b1261432f06b",
     "url": "/pomodoro/index.html"
   },
   {
-    "revision": "193575e2d8ea3e3f8ec6",
+    "revision": "ae65edc4180829ccf3a3",
     "url": "/pomodoro/css/app.326bd39c.css"
   }
 ];
--- pomodoro/service-worker.js
+++ pomodoro/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pomodoro/precache-manifest.1864c103a696078ec87f65d56bfbbba8.js"
+  "/pomodoro/precache-manifest.349a3d41a9c003456cd61c4f87465ada.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pomodoro"});
